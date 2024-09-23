2bfc8fe9068c56a45fad2c97e70cf348da6b0b57
porChe1223 <houshinri@gmail.com>
2024-09-24 05:48:19 +0900

    20240924-2___商品一覧・詳細画面作成

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -1,3 +1,84 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-</div>
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      {{ __('商品一覧') }}
+    </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
+        <div class="product-container">
+          @foreach($shops as $shop)
+          <div class="product-group">
+
+            <a href="{{ route('shops.show', $shop->id) }}">
+              <div class="product-item">
+                <img class="product-image" src="{{$shop->img_link}}" alt="商品画像">
+                <div class="product-info">
+                  <span>{{$shop->name}}</span><br>
+                  <span class="price">{{$shop->price}}円</span>
+                </div>
+                <a class='show_link' href="{{ route('shops.show', $shop->id) }}">詳細を見る</a>
+              </div>
+            </a>
+
+          </div>
+          @endforeach
+        </div>
+
+      </div>
+    </div>
+  </div>
+</x-app-layout>
+
+
+<style>
+  .product-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+    .product-group {
+      width: calc(33.33% - 10px); 
+    }
+      .product-item {
+        background-color: #ffffff;
+        color: black;
+        font-weight: bold;
+        border: 1px solid #ebebeb;
+        border-radius: 4px;
+        padding: 3px;
+        position: relative;
+        height: 360px;
+        outline: none;
+        box-shadow: 0 0px 2px rgba(0, 0, 0, 0.1);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 10px;
+      }
+      .product-item:hover {
+        background-color: #f3f3f3;
+      }
+        .product-image {
+          width: 100%;
+          object-fit: cover;
+        }
+        .product-info {
+          width: 100%;
+          position: absolute;
+          bottom: 25px;
+        }
+        .price{
+          color: red;
+        }
+      .show_link{
+        color: #3b82f6;
+      }
+      .show_link:hover{
+        color: #1d4ed8;
+      }
+</style>
--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -1,3 +1,113 @@
-<div>
-    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
-</div>
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      {{ __('商品詳細') }}
+    </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+          <div class="product-item">
+            <h class= "name">{{$shop->name}}</h>
+            <div class="product-docs">
+              <div class="product-docs-left">
+                <img class="product-image" src="{{$shop->img_link}}" alt="商品画像">
+              </div>
+              <div class="product-docs-right">
+                <div class="product-info">
+                  <span class= "description">{{$shop->description}}</span>
+                  <span class="price">{{$shop->price}}円</span>
+                  <span class="stock">残り{{$shop->stock}}個</span>
+                </div>
+                <button class="button">購入する</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class='back_link' href="{{ route('shops.index') }}">商品一覧に戻る</a>
+  </div>
+</x-app-layout>
+
+
+<style>  
+  .product-item{
+    display: flex;
+    flex-direction: column; 
+  }
+    .name{
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    .product-docs{
+      display: flex;
+      flex-direction: row;
+    }
+      .product-docs-left{
+        float: left;
+        display: flex;
+        flex-direction: column;
+        width: 250px;
+        margin: 10px;
+      }
+        .product-image{
+          width: 100%;
+          height: auto;
+          margin: 10px;
+          object-fit: cover;
+        }
+        .back_link{
+          position: absolute;
+          bottom: 0px;
+          font-size: 18px;
+          font-weight: bold;
+          color: #3b82f6;
+          margin: 30px;
+        }
+        .back_link:hover{
+          color: #1d4ed8;
+        }
+      .product-docs-right{
+        float: right;
+        display: flex;
+        flex-direction: column;
+        width: 550px;
+        margin: 10px;
+      }
+        .product-info{
+          text-align: left;
+          display: flex;
+          flex-direction: column;
+        }
+          .description{
+            margin: 10px;
+            height: 200px;
+          }
+          .price{
+            margin: 10px;
+            font-size: 24px;
+            color: red;
+          }
+          .stock{
+            margin: 20px;
+          }
+          .button{
+            background-color: #4299e1;
+            color: #ffffff;
+            font-weight: bold;
+            padding-top: 0.5rem;
+            padding-bottom: 0.5rem;
+            padding-left: 1rem;
+            padding-right: 1rem;
+            border-radius: 0.25rem;
+            outline: none;
+            box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);
+          }
+          .button:hover{
+            background-color: #2b6cb0;
+          }
+</style>
